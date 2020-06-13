b4fc48e0005cce18a1f5a217a2c68f8ce0477af2
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-06-13 22:27:32 +0500

    Class Library is uploaded

---
+++ EcommerceClassLibrary/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceClassLibrary
+{
+    public class ProductModel: BaseModel
+    {
+        public CatagoryModel Catagory { get; set; }
+        
+        public decimal Price { get; set; }
+    }
+}
