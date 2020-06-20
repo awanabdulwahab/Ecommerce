bb798e90a529aac937431195e1ace5e48b20a119
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-06-21 03:11:32 +0500

    Initial Project Setup is completed

---
+++ EcommerceDatabase/EcommerceStoreContext.cs
@@ -0,0 +1,21 @@
+﻿using EcommerceClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDatabase
+{
+    public class EcommerceStoreContext : DbContext
+    {
+        public EcommerceStoreContext() : base("ecommerce")
+        {
+
+        }
+        // Migration Concept in entity framework
+        public DbSet<CatagoryModel> Catagories { get; set; }
+        public DbSet<ProductModel> Products { get; set; }
+    }
+}
---
+++ EcommerceDatabase/Migrations/202006202125079_intialized.cs
@@ -0,0 +1,44 @@
+﻿namespace EcommerceDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intialized : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CatagoryModels",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.ProductModels",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Catagory_id = c.Int(),
+                    })
+                .PrimaryKey(t => t.id)
+                .ForeignKey("dbo.CatagoryModels", t => t.Catagory_id)
+                .Index(t => t.Catagory_id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ProductModels", "Catagory_id", "dbo.CatagoryModels");
+            DropIndex("dbo.ProductModels", new[] { "Catagory_id" });
+            DropTable("dbo.ProductModels");
+            DropTable("dbo.CatagoryModels");
+        }
+    }
+}
