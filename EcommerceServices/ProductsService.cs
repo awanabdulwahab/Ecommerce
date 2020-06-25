055248cb23747264760a048eeedc0046ebdd044b
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-06-25 16:44:09 +0500

    CRUD opeation using Ajax

---
+++ EcommerceServices/ProductsService.cs
@@ -0,0 +1,56 @@
+﻿using EcommerceClassLibrary;
+using EcommerceDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceServices
+{
+    public class ProductsService
+    {
+        public void SaveProduct(ProductModel product)
+        {
+            using(var context = new EcommerceStoreContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateProduct(ProductModel product)
+        {
+            using(var context = new EcommerceStoreContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        } 
+        public void DeleteProduct(int id)
+        {
+            using(var context = new EcommerceStoreContext())
+            {
+                //context.Entry(catagory).State = System.Data.Entity.EntityState.Deleted;
+                var product = context.Products.Find(id);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+
+        public List<ProductModel> GetProducts()
+        {
+            using(var context = new EcommerceStoreContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public ProductModel GetProduct(int id)
+        {
+            using(var context = new EcommerceStoreContext())
+            {
+                
+                return context.Products.Find(id);
+            }
+        }
+    }
+}
---
+++ EcommerceStore/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using EcommerceClassLibrary;
+using EcommerceServices;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerceStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductTable(string search)
+         {
+            var products = productsService.GetProducts();
+
+            if (!search.IsNullOrWhiteSpace())
+            {
+                products = products.Where(p => p.Name != null && p.Name.Contains(search)).ToList();
+
+            }           
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        // POST: Product
+        [HttpPost]
+        public ActionResult Create(ProductModel product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }  
+        
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var product = productsService.GetProduct(id);
+            return PartialView(product);
+        }
+        // POST: Product
+        [HttpPost]
+        public ActionResult Edit(ProductModel product)
+        {
+            productsService.UpdateProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+        
+        // POST: Product
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            productsService.DeleteProduct(id);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
